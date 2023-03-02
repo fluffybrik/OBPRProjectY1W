23098843671976922e8dcb09943be299b75d50bc
Skybr <Skybr@LAPTOP-LAMBBNRQ>
2023-03-02 16:42:18 -0500

    Add project files.

---
+++ OBPRProjectY1W/OBPRProjectY1W.cpp
@@ -0,0 +1,11 @@
+// OBPRProjectY1W.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "unga bunga bonanza\n";
+}
+
+// PROJECT TIME FELLASS!!!!
